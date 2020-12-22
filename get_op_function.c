d01005ec6333ef20b8040bd72a49c57a6eb08f22
Faith-qa <atienofaith12@gmail.com>
2020-12-22 22:17:17 +0300

    corrected function lay out

--- get_op_function.c
+++ get_op_function.c
@@ -3,8 +3,11 @@
 /**
  * get_op_func - select the correct operation function asked by user
  * @s: operator argument
+ *@line: struct containing line number
+ *@meta: struct containing the allocated memory
+ *
  * Return: function pointer corresponding to operator given
- */
+ *
 int (*get_op_func(char *s))(int)
 {
 	instruction_t ops[] = {
@@ -21,3 +24,20 @@ int (*get_op_func(char *s))(int)
 		i++;
 	return (ops[i].f);
 }
+*/
+void (*get_op_func(char *s))(stack_t **, unsigned int)
+ {
+  
+   instruction_t ops[] = {
+			  {"push", op_push},
+		{"pall", op_pall},
+		{"pint", op_pint},
+		{"pop", op_pop},
+		{"swap", op_swap},
+		{NULL, NULL}
+   };
+ unsigned int i = 0;
+while ((ops[i].opcode != NULL && *(ops[i].opcode) != *s))
+		i++;
+	return (ops[i].f);
+ }
