f25ae52012e368bd93463c6e2a32f18a8e5b548a
Emmastro <emurairi18@alastudents.org>
2020-12-28 07:43:26 +0100

    Fix error message

--- monty.c
+++ monty.c
@@ -40,11 +40,10 @@ int main(int argc, char const *argv[])
 		if (func == NULL)
 		{
 			/*TODO: Refactor: Edit more efifcient way to free memory on exit*/
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, line[0]);
 			free(line);
 			free_stack(stack);
 			free_lines(lines);
-
-			fprintf(stderr, "L%d: unknown instruction\n", line_number);
 			exit(EXIT_FAILURE);
 		}
 
